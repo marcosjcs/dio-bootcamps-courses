85747eca40125a12b352f6aac41ce51add8aa45e
marcosjcs <jcs.marcos13@gmail.com>
2021-10-20 15:54:31 -0300

    ChessFlix

--- Courses/09 - Recriando a Interface do Netflix/chessflix/js/owl/setup.js
+++ Courses/09 - Recriando a Interface do Netflix/chessflix/js/owl/setup.js
@@ -2,6 +2,9 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
     responsive:{
         0:{
             items:1
